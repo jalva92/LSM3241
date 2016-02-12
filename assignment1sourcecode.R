4990396ff46873248725ca33a63275f7c0de51b4
Jacob Alvarez <jacobjosiahalvarez@gmail.com>
2016-02-12 11:25:08 +0800

    assignment 1

---
+++ assignment1sourcecode.R
@@ -0,0 +1,25 @@
+# Loading the needed libraries
+library(affy)
+library(limma)
+library(magrittr)
+library(hgu133plus2.db)
+
+# Set directory path, read CEL files and normalise with rma
+setwd("~/lsm3241/GSE51395/celfiles")
+celfiles=ReadAffy()
+rmanormalized=rma(celfiles)
+
+# Delineating the experimental conditions
+diseasestatus=c(rep("Nontreated",10), 
+                rep("Knockdown",10))
+modelmatrix=model.matrix(~diseasestatus+0)
+colnames(modelmatrix)=c("Knockdown","Nontreated")
+contrastmatrix=makeContrasts(Nontreated-Knockdown, levels=modelmatrix) #Since knockdown effect is being studied
+
+# Fitting linear model and identifying significant DGE
+fit1=lmFit(rmanormalized, modelmatrix)
+contrastfit=contrasts.fit(fit1,contrastmatrix) 
+contrastfit=eBayes(contrastfit) 
+significanthits=topTable(contrastfit,p.value = 0.05) 
+
+probeMapping=select(hgu133plus2.db,columns=c("SYMBOL",”GENENAME”),keys = rownames(significanthits))
